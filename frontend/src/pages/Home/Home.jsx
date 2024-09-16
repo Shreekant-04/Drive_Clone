fea9ba1c52f6d6b3d28f2827521cc26842670a0e
RAJ TILAK SINGH <87015087+crackerghost@users.noreply.github.com>
2024-09-16 16:37:25 +0530

    completed dash

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
-import Navbar from '../Common/Navbar'
+import React from "react";
+import Navbar from "../Common/Navbar";
+import Sidebar from "../Common/Sidebar";
+import HomePage from "./HomePage";
 
 const Home = () => {
   return (
+    <>
+      <Navbar />
+       <div className="flex w-[100vw] h-[100vh]">
+        <Sidebar/>
+        <HomePage/>
 
-    <div>
-      <Navbar/>
-      Home</div>
-  )
-}
 
-export default Home
+
+      </div>
+    </>
+  );
+};
+
+export default Home;
