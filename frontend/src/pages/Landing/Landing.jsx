feab4f5a6b4ef8ee67d510f2081cdc0fd0ae45b0
Khushi <kaybesky89@gmail.com>
2024-09-17 22:06:31 +0530

    Plans component made

---
+++ frontend/src/pages/Landing/Landing.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Plans from './Plans'
+
+function Landing() {
+  return (
+    <>
+        <Plans/>
+    </>
+  )
+}
+
+export default Landing
--- frontend/src/routes/Routing.jsx
+++ frontend/src/routes/Routing.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Route, Routes} from "react-router-dom";
 import Home from "../pages/Home/Home";
+import Landing from "../pages/Landing/Landing";
 
 const Routing = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/landing" element={<Landing/>}/>
       </Routes>
     </div>
   );
