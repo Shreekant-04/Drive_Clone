e4c8fe1cfbd54b32521930bb045c2342cbd691cd
Shreyas <shreyastungar762@gmail.com>
2024-09-18 15:45:46 +0530

    added Footer and Testimonials into Landinng page

--- frontend/src/pages/Landing/Landing.jsx
+++ frontend/src/pages/Landing/Landing.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Partners from "./Partners";
 import Plans from "./Plans";
+import Footer from "./Footer";
+import Testimonials from "./Testimonials";
 
 function Landing() {
   return (
     <>
       <Partners />
       <Plans />
+      <Testimonials />
+      <Footer />
     </>
   );
 }
