29dfa39e55ce32f0eb9861024b35e981214f3d8a
Shreyas <shreyastungar762@gmail.com>
2024-09-21 01:45:11 +0530

    updated file model

--- backend/models/File.js
+++ backend/models/File.js
@@ -1,12 +1,13 @@
 const mongoose = require("mongoose");
 
 const fileSchema = new mongoose.Schema({
-  storedName: { type: String, required: true }, 
+  storedName: { type: String, required: true },
   fileName: { type: String, required: true },
   type: { type: String, required: true },
   size: { type: Number, required: true },
   lAccess: { type: Date, default: Date.now },
   lName: { type: String, required: false },
+  folderId: { type: mongoose.Schema.Types.ObjectId, ref: "Folder" },
   userId: { type: mongoose.Schema.Types.ObjectId, required: true },
 });
 
