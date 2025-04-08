bff5fa9ab5224d751905a5873486a8ca7071c62b
Shreekant <shreekant4062@gmail.com>
2025-04-09 01:13:42 +0530

    Update API endpoint to Vercel deployment and comment out localhost option

--- frontend/src/utils/api.jsx
+++ frontend/src/utils/api.jsx
@@ -1,4 +1,4 @@
-// const api = "https://driveclone-production.up.railway.app/api/";
-const api = "http://localhost:8080/api/";
+const api = "https://drive-clone-qvxq.vercel.app/api/";
+// const api = "http://localhost:8080/api/";
 // https://drive-clone-five.vercel.app/api/
 export default api;
